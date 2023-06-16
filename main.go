434f989a99a5a21174332d10546c65f6e564ed13
Lucas Galdino <lucas.galdino.silva@ccc.ufcg.edu.br>
2023-06-15 21:31:31 -0300

    inicia criação do servidor

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", ":8000")
+	if err != nil {
+		log.Println(err)
+	}
+	
+	fmt.Println("Servidor rodando")
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		handleRequest(conn)
+	} 
+}
+
+func handleRequest(conn net.Conn) {}
+
